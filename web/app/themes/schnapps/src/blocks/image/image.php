d92c5c40abddd61e1ba5e564ae6b4c92b41e4b48
Tom Gurney <=>
2023-06-23 09:10:03 +0100

    feat: glide image library

--- web/app/themes/schnapps/src/blocks/image/image.php
+++ web/app/themes/schnapps/src/blocks/image/image.php
@@ -4,6 +4,7 @@ namespace Giantpeach\Schnapps\Theme\Blocks\Image;
 
 use Giantpeach\Schnapps\Blocks\Block;
 use Giantpeach\Schnapps\Blocks\Interfaces\BlockInterface;
+use Giantpeach\Schnapps\Images\Images;
 
 class Image extends Block implements BlockInterface
 {
@@ -12,12 +13,18 @@ class Image extends Block implements BlockInterface
   public $width;
   public $height;
 
-  public function __construct($url, $alt, $width, $height)
+  public $image;
+  public $webp;
+  public $mobile;
+
+  public function __construct($url, $alt, $width, $height, $webp = null, $mobile = null)
   {
     $this->url = $url ?? null;
     $this->alt = $alt ?? '';
     $this->width = $width;
     $this->height = $height;
+    $this->webp = $webp;
+    $this->mobile = $mobile;
   }
 
   public static function getBlockName(): string
@@ -27,11 +34,27 @@ class Image extends Block implements BlockInterface
 
   public static function display(): void
   {
+    $imgField = get_field('image');
+
+    $imgUrl = Images::getInstance()->getGlideImageUrl($imgField['ID'], [
+      'w' => $imgField['width'],
+      'h' => $imgField['height'],
+      'fit' => 'crop',
+    ]);
+
+    $webp = Images::getInstance()->getGlideImageUrl($imgField['ID'], [
+      'w' => $imgField['width'],
+      'h' => $imgField['height'],
+      'fit' => 'crop',
+      'fm' => 'webp',
+    ]);
+
     $image = new Image(
-      get_field('image')['url'],
+      $imgUrl,
       get_field('image')['alt'],
       get_field('image')['width'],
-      get_field('image')['height']
+      get_field('image')['height'],
+      $webp
     );
     $image->render();
   }
--- web/app/themes/schnapps/src/blocks/image/template.php
+++
@@ -1,5 +0,0 @@
-<picture class="block">
-  <?php if ($this->url) : ?>
-    <img class="w-full !h-full object-cover" src="<?php echo $this->url ?>" alt="<?php echo $this->alt ?>" />
-  <?php endif; ?>
-</picture>
