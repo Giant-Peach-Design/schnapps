093a4f3716a5846316d0338d7460150d0c449533
Tom Gurney <=>
2023-12-01 13:36:45 +0000

    adds support for schnapps-framework package to move some of the more boilerplatey aspects out of the project

--- web/app/themes/schnapps/src/Schnapps.php
+++ web/app/themes/schnapps/src/Schnapps.php
@@ -2,50 +2,44 @@
 
 namespace Giantpeach\Schnapps\Theme;
 
-use Giantpeach\Schnapps\Config\Facades\Config;
-use Giantpeach\Schnapps\Theme\Blocks\Blocks;
-use Giantpeach\Schnapps\Images\Images;
+use Giantpeach\Schnapps\Framework\SchnappsFramework;
 use Giantpeach\Schnapps\Navigation\Navigation;
-use Giantpeach\Schnapps\Query\Cli\Cli as QueryCli;
-use Giantpeach\Schnapps\Theme\Routes\Api;
 
-class Schnapps
+class Schnapps extends SchnappsFramework
 {
 
   public function __construct()
   {
-    // Load Config Files
-    Config::load();
-
-    $this->setupTheme();
-    $this->setupFilters();
-
-    Images::getInstance();
-
-    // Load Blocks
-    new Blocks();
-
-    // Load API Routes
-    new Api();
-
-    // Load CLI Commands
-    new QueryCli();
+    parent::__construct();
   }
 
+  /**
+   * Sets up the theme.
+   *
+   * This method is called to initialize the theme and perform any necessary setup tasks.
+   * It extends the parent setupTheme() method and can be used to add custom actions.
+   * The parent class will call the following methods automatically:
+   * - registerPostTypes()
+   * - registerMenus()
+   * - stylesheets()
+   * - scripts()
+   * - blockEditorStylesheets()
+   * - blockEditorScripts()
+   *
+   * @return void
+   */
   public function setupTheme(): void
   {
-    add_action('init', [$this, 'registerPostTypes']);
-    add_action('init', [$this, 'registerMenus']);
+    parent::setupTheme();
 
-    add_action('init', [$this, 'createThemeOptionsPage']);
-
-    add_action('wp_enqueue_scripts', [$this, 'stylesheets']);
-    add_action('wp_enqueue_scripts', [$this, 'scripts']);
-
-    add_action('enqueue_block_editor_assets', [$this, 'blockEditorStylesheets']);
-    add_action('enqueue_block_editor_assets', [$this, 'blockEditorScripts']);
+    //e.g. add_action('init', [$this, 'thingToRunOnInit']);
   }
 
+  /**
+   * Registers custom post types.
+   *
+   * @return void
+   */
   public function registerPostTypes(): void
   {
     // Register custom post types here
@@ -64,62 +58,21 @@ class Schnapps
   }
 
   /**
-   * Create theme options page
+   * Set up the filters for the Schnapps class.
    *
-   * @return void
+   * This method extends the parent class's setupFilters method.
+   * It can be used to add custom filters.
+   * The parent class will call the following methods automatically:
+   * - acf/blocks/no_fields_assigned_message
+   * - upload_mimes
+   * - acf/load_field/name=post_type
+   * - acf/load_field/name=taxonomy
+   * - acf/load_field/name=image_sizes
+   * 
    */
-  public function createThemeOptionsPage(): void
+  public function setupFilters(): void
   {
-    if (function_exists('acf_add_options_page')) {
-      acf_add_options_page([
-        'page_title' => 'Theme Options',
-        'menu_title' => 'Theme Options',
-        'menu_slug' => 'theme-options',
-        'capability' => 'edit_posts',
-        'redirect' => false,
-      ]);
-    }
-  }
-
-  public function setupFilters()
-  {
-
-    add_filter('acf/blocks/no_fields_assigned_message', function () {
-      return 'This block contains no editable fields.';
-    });
-
-    add_filter('upload_mimes', function ($mimes) {
-      $mimes['svg'] = 'image/svg+xml';
-      return $mimes;
-    });
-
-    add_filter('acf/load_field/name=post_type', function ($field) {
-      foreach (get_post_types('', 'names') as $post_type) {
-        $field['choices'][$post_type] = $post_type;
-      }
-
-      // return the field
-      return $field;
-    });
-
-    add_filter('acf/load_field/name=taxonomy', function ($field) {
-      foreach (get_taxonomies('', 'names') as $taxonomy) {
-        $field['choices'][$taxonomy] = $taxonomy;
-      }
-
-      // return the field
-      return $field;
-    });
-
-    add_filter('acf/load_field/name=image_sizes', function ($field) {
-      $sizes = Config::get('image-sizes');
-
-      foreach ($sizes as $key => $imageSize) {
-        $field['choices'][$key] = $key;
-      }
-
-      return $field;
-    });
+    parent::setupFilters();
   }
 
   public function stylesheets()
