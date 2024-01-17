aca618248a8a93bd025b0da0b414d4a4b972398e
Tom Gurney <=>
2024-01-17 14:15:47 +0000

    dev: tweaks to test, composer update

--- web/app/themes/schnapps/src/tests/CreateApplication.php
+++ web/app/themes/schnapps/src/tests/CreateApplication.php
@@ -6,8 +6,7 @@ trait CreateApplication
 {
   public function createApplication()
   {
-    // get path to wp-config.php
-
+    // load wp environment
     require_once dirname(__DIR__, 6) . '/vendor/autoload.php';
     require_once dirname(__DIR__, 6) . '/config/application.php';
     require_once ABSPATH . 'wp-settings.php';
