fbc183c28fe34b54b204e37bb622a0433b08a27d
Tom Gurney <=>
2024-02-07 15:28:08 +0000

    feat: UX improvments to Column block

--- web/app/themes/schnapps/src/Blocks/Column/Column.php
+++ web/app/themes/schnapps/src/Blocks/Column/Column.php
@@ -10,97 +10,98 @@ use Giantpeach\Schnapps\Blocks\Traits\Spacing;
 
 class Column extends Block implements BlockInterface
 {
-
   use Prose;
 
+  /**
+   * An array of desktop width classes for the Column block.
+   *
+   * @var array
+   */
   private array $widthClasses = [
-    'lg:w-1/12',
-    'lg:w-2/12',
-    'lg:w-3/12',
-    'lg:w-4/12',
-    'lg:w-5/12',
-    'lg:w-6/12',
-    'lg:w-7/12',
-    'lg:w-8/12',
-    'lg:w-9/12',
-    'lg:w-10/12',
-    'lg:w-11/12',
-    'lg:w-12/12',
+    "lg:w-1/12",
+    "lg:w-2/12",
+    "lg:w-3/12",
+    "lg:w-4/12",
+    "lg:w-5/12",
+    "lg:w-6/12",
+    "lg:w-7/12",
+    "lg:w-8/12",
+    "lg:w-9/12",
+    "lg:w-10/12",
+    "lg:w-11/12",
+    "lg:w-12/12",
   ];
 
+  /**
+   * This private property represents an array of tablet width classes.
+   *
+   * @var array
+   */
   private array $tabletWidthClasses = [
-    'sm:w-1/12',
-    'sm:w-2/12',
-    'sm:w-3/12',
-    'sm:w-4/12',
-    'sm:w-5/12',
-    'sm:w-6/12',
-    'sm:w-7/12',
-    'sm:w-8/12',
-    'sm:w-9/12',
-    'sm:w-10/12',
-    'sm:w-11/12',
-    'sm:w-12/12',
+    "sm:w-1/12",
+    "sm:w-2/12",
+    "sm:w-3/12",
+    "sm:w-4/12",
+    "sm:w-5/12",
+    "sm:w-6/12",
+    "sm:w-7/12",
+    "sm:w-8/12",
+    "sm:w-9/12",
+    "sm:w-10/12",
+    "sm:w-11/12",
+    "sm:w-12/12",
   ];
 
-  private array $mobileWidthClasses = ['w-4/12', 'w-6/12', 'w-8/12', 'w-full'];
+  /**
+   * An array of CSS classes representing the width of the column on mobile devices.
+   *
+   * @var array
+   */
+  private array $mobileWidthClasses = ["w-4/12", "w-6/12", "w-8/12", "w-full"];
 
+  /**
+   * The classes for the column.
+   *
+   * @var Classes
+   */
   public Classes $columnClass;
 
-  public string $widthClass;
-  public bool $flexGrow = false;
-  public array $breakout = [];
+  public static string $blockName = "giantpeach/column";
 
-  public static string $blockName = 'giantpeach/column';
-
-  public function __construct($mobile = 4, $tablet = 6, $desktop = 6, $flexGrow = false, $display = [], $advancedControls = false, $breakout = [])
+  public function __construct()
   {
     parent::__construct();
-    $this->columnClass = new Classes();
-    $this->flexGrow = $flexGrow;
-    $widthClass = '';
-    if ($advancedControls) {
-      $this->widthClass = $widthClass = $this->widthClasses[$desktop - 1] . ' ' . $this->tabletWidthClasses[$tablet - 1] . ' ' . $this->mobileWidthClasses[$mobile - 1];
 
-      if ($breakout) {
-        $this->breakout = $breakout;
-      }
+    $this->columnClass = new Classes();
+
+    $flexGrow = get_field("fill_space") ?? false;
+
+    $desktopWidth = (int) get_field("desktop") ?? 6;
+    $tabletWidth = (int) get_field("tablet") ?? 6;
+    $mobileWidth = (int) get_field("mobile") ?? 4;
+
+    $desktopVisible = get_field("desktop_visible") ?? true;
+    $tabletVisible = get_field("tablet_visible") ?? true;
+    $mobileVisible = get_field("mobile_visible") ?? true;
+
+    // if fill_space is set to false then we want to use the width
+    // settings to determine the width of the column.
+    if (!$flexGrow) {
+      $this->columnClass->add($this->widthClasses[$desktopWidth - 1]);
+      $this->columnClass->add($this->tabletWidthClasses[$tabletWidth - 1]);
+      $this->columnClass->add($this->mobileWidthClasses[$mobileWidth - 1]);
+    } else {
+      $this->columnClass->add("flex-grow");
     }
 
-    $this->columnClass->add($widthClass);
-    $this->columnClass->add($this->flexGrow ? 'flex-grow' : '');
-    $this->columnClass->add($display['mobile'] ? 'block' : 'hidden');
-    $this->columnClass->add($display['tablet'] ? 'md:block' : 'md:hidden');
-    $this->columnClass->add($display['desktop'] ? 'lg:block' : 'lg:hidden');
+    $this->columnClass->add($desktopVisible ? "lg:block" : "lg:hidden");
+    $this->columnClass->add($tabletVisible ? "md:block" : "md:hidden");
+    $this->columnClass->add($mobileVisible ? "block" : "hidden");
   }
 
   public static function display(): void
   {
-    $mobile = get_field('mobile') ?? 4;
-    $tablet = get_field('tablet') ?? 6;
-    $desktop = get_field('desktop') ?? 6;
-    $flexGrow = get_field('fill_space') ?? false;
-    $display = get_field('column_visibility') ?? [
-      'mobile' => true,
-      'tablet' => true,
-      'desktop' => true,
-    ];
-    $advancedControls = get_field('advanced_controls') ?? false;
-
-    $breakout = [
-      'left' => get_field('break_out_of_container_left_side') ?? '',
-      'right' => get_field('break_out_of_container_right_side') ?? '',
-    ];
-
-    $column = new Column(
-      $mobile,
-      $tablet,
-      $desktop,
-      $flexGrow,
-      $display,
-      $advancedControls,
-      $breakout
-    );
+    $column = new Column();
     $column->render();
   }
 }
